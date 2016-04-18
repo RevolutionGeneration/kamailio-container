3dc88c88a9fd6f129eaca7dff3ddc41dcb0110e8
Michal Tesar <michal@revolutiongeneration.co.uk>
2016-04-18 11:40:36 +0100

    log to /var/log/kamailio.log

--- start.sh
+++ start.sh
@@ -8,4 +8,10 @@ if [ "$FREESWITCH_BACKUP" != "" ] ; then
     echo "2 sip:$FREESWITCH_BACKUP 0" >> /etc/kamailio/dbtext/dispatcher;
 fi
 
+echo "local0.* -/var/log/kamailio.log" >> /etc/rsyslog.d/50-default.conf;
+touch /var/log/kamailio.log;
+chown syslog /var/log/kamailio.log;
+chgrp adm /var/log/kamailio.log;
+service rsyslog restart;
+
 kamailio -f /etc/kamailio/kamailio.cfg
